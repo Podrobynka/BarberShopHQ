3333b0767785d5f26cbd6ea6f53c1c37c8330929
Podrobynka <yfgurda@gmail.com>
2018-10-22 13:20:52 +0300

    delite  excessive controlers

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/contrib'
-require 'sinatra/reloader' #if development?
+require 'sinatra/reloader' # if development?
 require 'sinatra/activerecord'
-require 'pony'
-require 'sqlite3'
 
-set :database, "sqlite3:barbershop.db"
+set :database, 'sqlite3:barbershop.db'
 
 class Client < ActiveRecord::Base
 end
 
 get '/' do
-  erb :main
+  erb 'Hello'
 end
