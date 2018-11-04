bb746fbc56d122e1fcf1e418ac24de5cf682ea72
Podrobynka <yfgurda@gmail.com>
2018-11-04 20:24:24 +0200

    fix table name in migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_10_22_110841) do
-
-  create_table "barber", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "barbers", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "client", force: :cascade do |t|
+  create_table "clients", force: :cascade do |t|
     t.text "name"
     t.text "phone"
     t.text "datestamp"
